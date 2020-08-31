4cadf29e2522cf1862137258b717cfcd5732c130
Vanessa <va.au@gmx.at>
2020-08-31 09:23:33 +0200

    rdy for review

---
+++ src/WorkFreeTime.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class WorkFreeTime {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int time = scanner.nextInt();
+
+        if (time >= 12 && time <= 13) {
+            System.out.println("It's lunch break.");
+        } else if (time >= 8 && time <= 16) {
+            System.out.println("It's work time.");
+        }
+    }
+}
